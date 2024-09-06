b3d97fb4586d5f4d1bf67b50ca36f03bcb1479d9
sol <nathf1005@naver.com>
2024-09-06 22:09:17 +0900

    현재 슬라이드값을 상태관리 설정후 그에 따른 스타일 지정

--- src/commponents/Carousel.jsx
+++ src/commponents/Carousel.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./Carousel.css"
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 export const Carousel = ({ data }) => {
+    const [slide, setSlide] = useState(0)
+
     return (
         <div className='carousel'>
-            <BsArrowLeftCircleFill className='arrow arrow-left'/>
+            <BsArrowLeftCircleFill className='arrow arrow-left' />
             {
                 data.map((item, idx) => {
-                    return <img src={item.src} alt={item.src} key={idx} className='slide' />
+                    return <img src={item.src} alt={item.src} key={idx} className={slide === idx ? "slide" : "slide-hidden"} />
                 })
             }
-            <BsArrowRightCircleFill className='arrow arrow-right'/>
+            <BsArrowRightCircleFill className='arrow arrow-right' />
             <span className='indicators'>
-                {data.map((_, idx)=>{
-                    return <button key={idx} onClick={null} className='indicator'></button>
+                {data.map((_, idx) => {
+                    return <button key={idx} onClick={null} className={slide === idx ? "indicator" : "indicator indicator-inactive"} ></button>
                 })}
             </span>
         </div>
